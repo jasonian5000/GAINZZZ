e9009fe5c079ac67d411b432df18a6b35b42b50a
Emgula96 <ethan_gula@yahoo.com>
2022-09-13 16:37:44 -0400

    pagination fix

--- client/src/Components/Exercises/ExerciseCard.jsx
+++ client/src/Components/Exercises/ExerciseCard.jsx
@@ -4,11 +4,10 @@ import { Button, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import '../../css/exerciseCard.css'
 
-const ExerciseCard = () => {
-  const searchResults = useSelector((state) => state.search?.searchResults);
+const ExerciseCard = (props) => {
   return (
     <div className="cardContainer">
-      {searchResults?.map((exercise, index) => (
+      {props.current?.map((exercise, index) => (
      <Link key={index} className="exerciseCard" to={`/exercise/${exercise.id}`}>   
           <Typography ml="21px" color="black" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize">
             {exercise.name}
