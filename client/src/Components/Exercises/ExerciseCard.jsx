48bc44e303da1c746ea1517197db46142ba313cf
Jason <jason.blunck@gmail.com>
2022-09-13 15:04:16 -0500

    add to favorites function

--- client/src/Components/Exercises/ExerciseCard.jsx
+++ client/src/Components/Exercises/ExerciseCard.jsx
@@ -1,27 +1,44 @@
-import { useSelector } from "react-redux";
-import React from "react";
-import { Button, Stack, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux'
+import React from 'react'
+import { Button, Stack, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 import '../../css/exerciseCard.css'
-import { addToFavorites } from "../../actions/addToFavorites";
+import { addToFavorites } from '../../actions/supabase_client'
 
 const ExerciseCard = () => {
-  const searchResults = useSelector((state) => state.search?.searchResults);
-  return (
-    <div className="cardContainer">
-      {searchResults?.map((exercise, index) => (
-     <Link key={index} className="exerciseCard" to={`/exercise/${exercise.id}`}>   
-          <Typography ml="21px" color="black" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize">
-            {exercise.name}
-          </Typography>
-          <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-          <Stack>
-            <Button onclick={() =>{addToFavorites()}}>Add to Workout</Button>
-          </Stack>
-        </Link>
-      ))}
-    </div>
-  );
-};
+    const searchResults = useSelector(state => state.search?.searchResults)
+    return (
+        <div className="cardContainer">
+            {searchResults?.map((exercise, index) => (
+                <div key={index} className="exerciseCard">
+                    <Typography
+                        ml="21px"
+                        color="black"
+                        fontWeight="bold"
+                        mt="11px"
+                        pb="10px"
+                        textTransform="capitalize"
+                    >
+                        {exercise.name}
+                    </Typography>
+                    <img
+                        src={exercise.gifUrl}
+                        alt={exercise.name}
+                        loading="lazy"
+                    />
+                    <Stack>
+                        <Button
+                            onClick={() => {
+                                addToFavorites(exercise.id)
+                            }}
+                        >
+                            Add to Favorites
+                        </Button>
+                    </Stack>
+                </div>
+            ))}
+        </div>
+    )
+}
 
-export default ExerciseCard;
+export default ExerciseCard
