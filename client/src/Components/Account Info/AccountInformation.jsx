0849fc8bd7460de922969d8f0016fa55d11a77bc
1ShoukR <rahminshoukoohi@gmail.com>
2022-09-13 15:14:31 -0400

    added css for account info

--- client/src/Components/Account Info/AccountInformation.jsx
+++ client/src/Components/Account Info/AccountInformation.jsx
@@ -21,85 +21,99 @@ useEffect(
 );
 
   return (
-    <div>
-        <div>
-        <h1>Please enter your personal information</h1>
-        <div>
-            <form className="accountInformationForm">
-            <div>
-                <label htmlFor="height">Height</label>
-                <input
-                type="text"
-                className="height"
-                placeholder="Enter your height"
-                />
-            </div>
-            <div>
-                <label htmlFor="gender">gender</label>
-                <input
-                type="text"
-                className="gender"
-                placeholder="Enter your gender"
-                />
-            </div>
-            <div>
-                <label htmlFor="weight">weight</label>
-                <input
-                type="text"
-                className="weight"
-                placeholder="Enter your weight"
-                />
-            </div>
-            <div>
-                <label htmlFor="bmi">
-                bmi. (If you do not know it, leave it blank.)
-              </label>
-              <input type="text" className="bmi" placeholder="Enter your bmi" />
+      <div className="accountInformationContainer">
+          <div className="accountInformationForm">
+              <h1 className="title">Please enter your personal information</h1>
               <div>
-                <label htmlFor="age">age</label>
-                <input type="text" className="age" placeholder="Enter your age" />
+                  <form>
+                      <div>
+                          <label htmlFor="height">Height</label>
+                          <input
+                              type="text"
+                              className="height"
+                              placeholder="Enter your height"
+                          />
+                      </div>
+                      <div>
+                          <label htmlFor="gender">gender</label>
+                          <input
+                              type="text"
+                              className="gender"
+                              placeholder="Enter your gender"
+                          />
+                      </div>
+                      <div>
+                          <label htmlFor="weight">weight</label>
+                          <input
+                              type="text"
+                              className="weight"
+                              placeholder="Enter your weight"
+                          />
+                      </div>
+                      <div>
+                          <label htmlFor="bmi">
+                              bmi. (If you do not know it, leave it blank.)
+                          </label>
+                          <input
+                              type="text"
+                              className="bmi"
+                              placeholder="Enter your bmi"
+                          />
+                          <div>
+                              <label htmlFor="age">age</label>
+                              <input
+                                  type="text"
+                                  className="age"
+                                  placeholder="Enter your age"
+                              />
+                          </div>
+                          <div>
+                              <label htmlFor="bodyfat">
+                                  bodyfat percentage (If you do not know it,
+                                  leave it blank.)
+                              </label>
+                              <input
+                                  type="text"
+                                  className="bodyfat"
+                                  placeholder="Enter your bodyfat percent"
+                              />
+                          </div>
+                          <div>
+                              <label htmlFor="totalBurnedCalories">
+                                  total burned calories. (If you do not know it,
+                                  leave it blank.)
+                              </label>
+                              <input
+                                  type="text"
+                                  className="totalBurnedCalories"
+                                  placeholder="Enter your total burned calories"
+                              />
+                          </div>
+                          <div>
+                              <label htmlFor="personalTrainer">
+                                  select personal trainer
+                                  <select value={value} onChange={handleChange}>
+                                      {trainerDropDownList.map(trainer => (
+                                          <option
+                                              key={trainer.id}
+                                              value={trainer.id}
+                                          >
+                                              {trainer.ptName}
+                                          </option>
+                                      ))}
+                                  </select>
+                              </label>
+                          </div>
+                          <button onClick={e => sendAccountInformation(e)}>
+                              Submit
+                          </button>
+                          <button>Update</button>
+                      </div>
+                  </form>
               </div>
-              <div>
-                <label htmlFor="bodyfat">
-                    bodyfat percentage (If you do not know it, leave it blank.)
-                </label>
-                <input
-                  type="text"
-                  className="bodyfat"
-                  placeholder="Enter your bodyfat percent"
-                />
-                </div>
-                <div>
-                <label htmlFor="totalBurnedCalories">
-                    total burned calories. (If you do not know it, leave it
-                    blank.)
-                </label>
-                <input
-                  type="text"
-                  className="totalBurnedCalories"
-                  placeholder="Enter your total burned calories"
-                />
-                </div>
-                <div>
-                <label htmlFor="personalTrainer">
-                  select personal trainer
-                  <select value={value} onChange={handleChange}>
-                    {trainerDropDownList.map((trainer) => (
-                      <option key={trainer.id} value={trainer.id}>
-                        {trainer.ptName}
-                        </option>
-                    ))}
-                    </select>
-                </label>
-                </div>
-                <button onClick={(e) => sendAccountInformation(e)}>Submit</button>
-                <button>Update</button>
-            </div>
-            </form>
-        </div>
-        </div>
-    </div>
-    );
+          </div>
+      </div>
+  )
 };
 
 export default AccountInformation;
