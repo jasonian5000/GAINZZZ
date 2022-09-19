de60abae3064dd54552558a6fbb262abc06d7ce8
Jason <jason.blunck@gmail.com>
2022-09-19 13:30:49 -0500

    added start script to server

--- client/src/actions/searchExercises_client.js
+++ client/src/actions/searchExercises_client.js
@@ -1,11 +1,11 @@
 export const searchExercises = async (searchInput, dispatch) => {
   let body = { "searchInput": searchInput }
-  let search = await fetch("http://localhost:3001/search", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
+  let search = await fetch('https://gainzzzz.herokuapp.com/search', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
   })
   let results = await search.json()
   console.log(results);
