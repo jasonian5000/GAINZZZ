208fac4e349036c3ec28721c885cc087cdcdbcbc
1ShoukR <rahminshoukoohi@gmail.com>
2022-09-21 14:39:41 -0400

    Update Features.jsx

--- client/src/Components/General Layout/Features.jsx
+++ client/src/Components/General Layout/Features.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import '../../css/homepage.css'
 
 const Features = (props) => {
+  console.log(props)
   return (
       <div className='a-box'>
           <div className='a-b-img'>
               <img src={props.image} alt=''/>
           </div>
           <div className='a-b-text'>
-              <h2>{props.title}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, odio.</p>
           </div>
     </div>
