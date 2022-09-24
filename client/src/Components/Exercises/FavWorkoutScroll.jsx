523a4d2f0636af0ab3953421937e1c12338910e0
Jason <jason.blunck@gmail.com>
2022-09-24 09:14:32 -0500

    removed unused code

--- client/src/Components/Exercises/FavWorkoutScroll.jsx
+++ client/src/Components/Exercises/FavWorkoutScroll.jsx
@@ -1,39 +1,17 @@
-import React, { useContext } from 'react'
-import { Stack, Typography } from '@mui/material'
+import React from 'react'
+import { Stack } from '@mui/material'
 import { useSelector } from 'react-redux'
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+import { ScrollMenu } from 'react-horizontal-scrolling-menu'
 import '../../css/hideScrollBar.css'
-// import RightArrowIcon from '../../assets/right-arrow.png'
-// import LeftArrowIcon from '../../assets/left-arrow.png'
 import FavoritesCard from './FavoritesCard'
 import { Box } from '@mui/system'
 
-// const LeftArrow = () => {
-//     const { scrollPrev } = useContext(VisibilityContext)
-
-//     return (
-//         <Typography onClick={() => scrollPrev()} className="right-arrow">
-//             <img src={LeftArrowIcon} alt="right-arrow" />
-//         </Typography>
-//     )
-// }
-
-// const RightArrow = () => {
-//     const { scrollNext } = useContext(VisibilityContext)
-
-//     return (
-//         <Typography onClick={() => scrollNext()} className="left-arrow">
-//             <img src={RightArrowIcon} alt="right-arrow" />
-//         </Typography>
-//     )
-// }
-
 const FavWorkoutScroll = () => {
-        const favWorkouts = useSelector(
-            state => state.favoriteWorkouts.favoriteWorkouts
-        )
+    const favWorkouts = useSelector(
+        state => state.favoriteWorkouts.favoriteWorkouts
+    )
     return (
-        <div className='scroll-container'>
+        <div className="scroll-container">
             <Box sx={{ width: '75%' }}>
                 <ScrollMenu>
                     <div id="favorites-container">
