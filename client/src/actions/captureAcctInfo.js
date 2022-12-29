23fccbc883be55b2965dbe48b66fb5fb89d06f0c
Jason <jason.blunck@gmail.com>
2022-12-28 19:16:06 -0600

    refined two actions

--- client/src/actions/captureAcctInfo.js
+++ client/src/actions/captureAcctInfo.js
@@ -1,18 +1,17 @@
 const captureAcctInfo = (e, info) => {
     e.preventDefault()
     let height =
-        e.target.form[0].value === '' ? info[0].height : e.target.form[0].value
+        e.target.form[0].value ? e.target.form[0].value : info[0].height
     let weight =
-        e.target.form[2].value === '' ? info[0].weight : e.target.form[2].value
+        e.target.form[2].value ? e.target.form[2].value : info[0].weight
     let age =
-        e.target.form[4].value === '' ? info[0].age : e.target.form[4].value
+        e.target.form[4].value ? e.target.form[4].value : info[0].age
     let gender =
-        e.target.form[6].value === '' ? info[0].gender : e.target.form[6].value
+        e.target.form[6].value ? e.target.form[6].value : info[0].gender
 
     let personalTrainer =
-        e.target.form[8].value === ''
-            ? info[0].personalTrainer
-            : e.target.form[8].value
+        e.target.form[8].value ? e.target.form[8].value : info[0].personalTrainer
+        
     let input = {
         height,
         weight,
